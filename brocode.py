49d5956ffb9d978d46ddc8f036ca8c4edf2a05cb
kaung-myat-thu <2962000kaungmyatthu@gmail.com>
2020-03-10 01:05:46 -0400

    Color mode for windows is remove DisplayInfo content is change according to the OS

--- brocode.py
+++ brocode.py
@@ -80,9 +80,9 @@ def main(mode):
 
 
 def DisplayUsage():
-        print(txtcolors.WARNING + "Usage : {} [-d or -e] [encryption_key] [content] [-v (optional)]".format(sys.argv[0]))
+        print(txtcolors.WARNING + "Usage : python {} [-d or -e] [encryption_key] [content] [-v (optional)]".format(sys.argv[0]))
         print("-d : Decode. \n-e : Encode.\n-v : verbose true , default is false")
-        print("Example : {} -e 'castle' 'pls make me secure'".format(sys.argv[0]) + txtcolors.RESET)
+        print("Example : python {} -e 'castle' 'pls make me secure'".format(sys.argv[0]) + txtcolors.RESET)
         
 
 def Encode(content, key): ####################### Encode Function Defination : This function is for encoding option ################################
